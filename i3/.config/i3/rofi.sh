54eb6a89e95ebfca65762f52e6bda35effeaf173
SlyCedix <slycedix@gmail.com>
2017-04-17 16:53:02 -0400

    Initial Commit

---
+++ i3/.config/i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rofi -show run -font "Input Mono 14" -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "Input Mono Bold 16" -padding 10 -separator-style none -opacity 80 -hide-scrollbar -width 50% -height 30%
