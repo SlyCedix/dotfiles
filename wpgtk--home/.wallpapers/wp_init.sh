05a3b2198640145223157e8a275da3bb02b473fa
SlyCedix <slycedix@gmail.com>
2017-04-26 23:02:58 -0400

    Begin complete rewrite of dotfiles, ditch wpgtk?

---
+++ wpgtk--home/.wallpapers/wp_init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wpcscript change misty-mountain-dual.png && xrdb -merge /home/sly/.wallpapers/.misty-mountain-dual.png.Xres
--- wpgtk/.wallpapers/wp_init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wpcscript change nature.png && xrdb -merge /home/sly/.wallpapers/.nature.png.Xres
