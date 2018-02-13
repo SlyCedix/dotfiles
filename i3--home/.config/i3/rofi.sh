e443158ecf02e63dbf462c3eae505f6cd99a5288
SlyCedix <eric.grogans98@gmail.com>
2018-02-12 21:58:02 -0500

    make rofi look better

--- i3--home/.config/i3/rofi.sh
+++ i3--home/.config/i3/rofi.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
+FG=$(xrdb -query -all | grep '*foreground' | sed 's/^[^#]*#/#/')
+BG=$(xrdb -query -all | grep '*background' | sed 's/^[^#]*#/#/')
+FGALT=$(xrdb -query -all | grep '*color20' | sed 's/^[^#]*#/#/')
+N=$(xrdb -query -all | grep '*color4' | sed 's/^[^#]*#/#/')
+A=$(xrdb -query -all | grep '*color2' | sed 's/^[^#]*#/#/')
+U=$(xrdb -query -all | grep '*color9' | sed 's/^[^#]*#/#/')
 
-rofi -show run -font "Input Mono 12" -location 0 -lines 6 -bw 0 -font "Input Mono Bold 14" -padding 10 -separator-style none -opacity 80 -hide-scrollbar -width 50% -height 30%
+rofi -show run -modi run -location 0 -width 30 \
+		 -lines 8 -line-margin 0 -line-padding 1 -padding 1\
+		 -separator-style none -font "input 10" -columns 1 -bw 0 \
+		 -disable-history \
+		 -hide-scrollbar \
+		 -color-window "$BG, $BG, $FG" \
+		 -color-normal "$FG, $BG, $FG, $N, $FGALT" \
+		 -color-active "$BG, $FGALT, $BG, $A, $FGALT" \
+		 -color-urgent "$BG, $FGALT, $BG, $U, $FGALT" \
+		 -kb-row-select "Tab" -kb-row-tab ""
+
+
