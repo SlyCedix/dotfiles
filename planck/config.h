2d6e8abffdabc97806b6a825c1417a57051bb9da
slycedix <slycedix@github.com>
2018-10-02 00:38:48 -0400

    retab

--- planck/config.h
+++ planck/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x6060
 #define MANUFACTURER    OLKB
 #define PRODUCT         Planck
-#define DESCRIPTION			Rev. 6 Planck Keyboard
+#define DESCRIPTION     Rev. 6 Planck Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 4
