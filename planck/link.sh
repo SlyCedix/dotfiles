24e926280355625aa40b17a1eb03a1cd073661d8
SlyCedix <SlyCedix@gmail.com>
2018-10-02 01:54:29 -0400

    Add force to link.sh to overwrite old links

--- planck/link.sh
+++ planck/link.sh
@@ -2,6 +2,6 @@
 
 mkdir -p qmk_firmware/keyboards/planck/keymaps/slycedix
 
-ln -s $(pwd)/config.h qmk_firmware/keyboards/planck/keymaps/slycedix/
-ln -s $(pwd)/keymap.c qmk_firmware/keyboards/planck/keymaps/slycedix/
-ln -s $(pwd)/rules.mk qmk_firmware/keyboards/planck/keymaps/slycedix/
+ln -sf $(pwd)/config.h qmk_firmware/keyboards/planck/keymaps/slycedix/
+ln -sf $(pwd)/keymap.c qmk_firmware/keyboards/planck/keymaps/slycedix/
+ln -sf $(pwd)/rules.mk qmk_firmware/keyboards/planck/keymaps/slycedix/
