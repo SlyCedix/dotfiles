747683e9f03c30977a0a9c28693c9d0d500dcf2e
cyn <SlyCedix@users.noreply.github.com>
2025-01-08 10:22:04 -0500

    Remove pauses from hooks

--- Hooks/glazewm_windows/pre.bat
+++ Hooks/glazewm_windows/pre.bat
@@ -1,9 +1,5 @@
-pause
-
 mkdir %USERPROFILE%\.glzr
 mkdir %USERPROFILE%\.glzr\glazewm
 mkdir %USERPROFILE%\.glzr\zebar
-
-pause
 
 exit /b 0
--- Hooks/zebar_windows/pre.bat
+++ Hooks/zebar_windows/pre.bat
@@ -1,14 +1,9 @@
-pause
-
 mkdir %USERPROFILE%\.glzr
 mkdir %USERPROFILE%\.glzr\glazewm
 mkdir %USERPROFILE%\.glzr\zebar
 
-pause
-
 cd /d %~dp0
 cd neobrutal-zebar
-git pull
 call npm i
 call npm run build
 call rmdir /s /q ..\..\..\Configs\zebar_windows\.glzr\zebar\neobrutal 
