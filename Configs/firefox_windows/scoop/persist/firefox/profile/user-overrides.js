d5a9f55953085d30ea82a252712e452c622f2aa6
cyn <SlyCedix@users.noreply.github.com>
2025-01-16 13:12:54 -0500

    Use arkenfox/user.js for base prefrences

---
+++ Configs/firefox_windows/scoop/persist/firefox/profile/user-overrides.js
@@ -0,0 +1,10 @@
+// Enable user styling
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("svg.context-properties.content.enabled", true);
+
+// Extra settings
+user_pref("browser.startup.homepage", "https://slycedix.github.io/Bento");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
--- Hooks/firefox_windows/~pre.ps1
+++ Hooks/firefox_windows/~pre.ps1
@@ -1,1 +1,6 @@
 Scoop-Up extras firefox
+cd $PSScriptRoot\..\..\Configs\firefox_windows\scoop\persist\firefox\profile
+
+irm https://github.com/arkenfox/user.js/raw/master/updater.bat > "updater.bat"
+
+& ".\updater.bat" -unattended
