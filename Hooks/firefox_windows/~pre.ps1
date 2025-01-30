84d74e48acd4ad7435460fb7af424c23182fcade
cyn <SlyCedix@users.noreply.github.com>
2025-01-30 17:03:45 -0500

    update firefox config

--- Hooks/firefox_windows/~pre.ps1
+++ Hooks/firefox_windows/~pre.ps1
@@ -2,5 +2,6 @@ Scoop-Up extras firefox
 cd $PSScriptRoot\..\..\Configs\firefox_windows\scoop\persist\firefox\profile
 
 irm https://github.com/arkenfox/user.js/raw/master/updater.bat > "updater.bat"
+irm https://github.com/arkenfox/user.js/raw/master/prefsCleaner.bat > "prefsCleaner.bat"
 
 & ".\updater.bat" -unattended
